530e3e5a3f3b46be7e152622aca6bb06a172e14a
hjun1052 <hjun7079@gmail.com>
2026-05-26 17:51:23 +0900

    Add DimiCheck Mac cask

---
+++ Casks/dimicheck-mac.rb
@@ -0,0 +1,15 @@
+cask "dimicheck-mac" do
+  version "0.1.0"
+  sha256 "8fc4c7f4f9a5740b38488323dc147560fdbafc09fdbf6bf1febca3c8bb65f5fd"
+
+  url "https://github.com/DimiCheck/Dimicheck-mac/releases/download/v#{version}/DimiCheck-Mac-#{version}.dmg"
+  name "DimiCheck Mac"
+  desc "Menu bar companion app for DimiCheck"
+  homepage "https://dimicheck.com/mac.html"
+
+  app "DimiCheck Mac.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.dimicheck.mac.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,13 @@
+# Homebrew Tap for DimiCheck
+
+Install DimiCheck Mac:
+
+```bash
+brew install --cask dimicheck/dimicheck/dimicheck-mac
+```
+
+If the tap is already added:
+
+```bash
+brew install --cask dimicheck-mac
+```
