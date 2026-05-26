32c15b50d9611fb27fa3fa2f1433e30380c3b4d4
hjun1052 <hjun7079@gmail.com>
2026-05-26 18:08:22 +0900

    Update cask checksum for styled dmg

--- Casks/dimicheck-mac.rb
+++ Casks/dimicheck-mac.rb
@@ -1,6 +1,6 @@
 cask "dimicheck-mac" do
   version "0.1.0"
-  sha256 "8fc4c7f4f9a5740b38488323dc147560fdbafc09fdbf6bf1febca3c8bb65f5fd"
+  sha256 "afa4d03fd1e157c7e454fb44f120b5781bd902005d95d755cd35724b833bb72d"
 
   url "https://github.com/DimiCheck/Dimicheck-mac/releases/download/v#{version}/DimiCheck-Mac-#{version}.dmg",
       verified: "github.com/DimiCheck/Dimicheck-mac/"
